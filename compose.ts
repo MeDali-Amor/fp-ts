9ba97f860bb2cde5617b2bfe18ec406efb92bbad
Mohamed Ali AMOR <amormedali92@gmail.com>
2025-02-06 20:46:44 +0100

    compose

--- compose.ts
+++ compose.ts
@@ -1,18 +1,21 @@
-export const compose =
-    <T, K, V>(f: (x: K) => V, g: (x: T) => K) =>
-    (x: T): V => {
-        return f(g(x));
-    };
-const compose2 = <T, K, V>(f: (x: K) => V, g: (x: T) => K) => {
-    return (x: T) => f(g(x));
+type Incremrnt = (x: number) => number;
+type NumToString = (x: number) => string;
+type Compose = <A, B, C>(f: (y: B) => C, g: (x: A) => B) => (x: A) => C;
+
+const increment: Incremrnt = (x) => x + 1;
+
+const numToString: NumToString = (x) => x.toString();
+
+const incrementToString = (x: number): string => {
+    return numToString(increment(x));
 };
 
-const g = (n: number) => {
-    return n + 1;
+const compose: Compose = (f, g) => {
+    return (x) => f(g(x));
 };
-const f = (n: number): string => {
-    return n.toString();
-};
-const c = compose(f, g)(6);
-const c2 = compose2(f, g)(7);
-console.log(c, c2);
+
+const increment_then_tostring = compose(numToString, increment);
+
+const a = increment_then_tostring(12);
+const b = incrementToString(12);
+console.log(a, b);
