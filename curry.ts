036ecee4c8f594393b1b4dcffb0f6ae816285614
Mohamed Ali AMOR <amormedali92@gmail.com>
2025-02-07 20:42:43 +0100

    curry

--- curry.ts
+++ curry.ts
@@ -2,18 +2,24 @@ const normalSum = (x: number, y: number): number => {
     return x + y;
 };
 
-const sum =
-    (x: number) =>
-    (y: number): number => {
-        return x + y;
-    };
-const increment: (x: number) => number = sum(1);
+const sum = (x: number) => (y: number) => x + y;
 
-const curry =
-    <T, K, V>(f: (x: T, y: K) => V) =>
-    (a: T) =>
-    (b: K): V =>
-        f(a, b);
 
-const sum2 = curry(normalSum);
-console.log(sum2(7)(5));
+const increment = (x: number) => sum(1)(x);
+/** equivalent to : point-free style, e.i no explicit mention of any parameter*/
+const increment2 = sum(1);
+
+const plusTwo =(x:number)=> sum(2)(x);
+/** equivalent to */
+const plusTwo2 = sum(2);
+
+type Curry2 = <A,B,C>(f:(a:A,b:B)=>C) => (a:A)=>(b:B)=>C;
+
+const curry:Curry2 = (f)=>(a)=>(b)=>f(a,b)
+
+const curriedSum = curry(normalSum);
+
+const increment3 = curriedSum(1);
+
+
+
